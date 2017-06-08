4e98067400cdc75e9a7ca9e1e8858498998c290e
Bukhari Muslim <bm.bukharimuslim@gmail.com>
2017-06-08 15:25:05 +0700

    initialize API with Laravel 5.4

---
+++ API/app/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password', 
+        'nama', 
+        'gender', 
+        'address', 
+        'bornPlace', 
+        'bornDate', 
+        'phone',
+        'city',
+        'religion',
+        'location',
+        'userType',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ API/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nama');
+            $table->tinyInteger('gender');
+            $table->text('address')->nullable();
+            $table->string('bornPlace');
+            $table->date('bornDate');
+            $table->string('phone');
+            $table->string('city');
+            $table->tinyInteger('religion');
+            $table->string('location');
+            $table->tinyInteger('userType');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
