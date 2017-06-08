56aa9596e82f4fef2a840e0ae82c687bb51e39e5
Achmad_Dzaki <131113832@students.mikroskil.ac.id>
2017-06-08 16:58:55 +0700

    add tampilan menu lainnya

--- Mobile-Member/app/src/main/java/com/TA/MVP/appmobilemember/View/Fragment/FragmentLainnya.java
+++ Mobile-Member/app/src/main/java/com/TA/MVP/appmobilemember/View/Fragment/FragmentLainnya.java
@@ -1,10 +1,14 @@
 package com.TA.MVP.appmobilemember.View.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.TA.MVP.appmobilemember.R;
 
@@ -13,11 +17,69 @@ import com.TA.MVP.appmobilemember.R;
  */
 
 public class FragmentLainnya extends Fragment {
+    private ImageView imageprofile, imagewallet, imagepengaturan, imagebantuan, imageketentuan, imagelogout;
+    private TextView txtprofile, txtwallet, txtpengaturan, txtbantuan, txtketentuan, txtlogout;
+    private Context context;
+
     public FragmentLainnya() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lainnya, container, false);
+        View _view = inflater.inflate(R.layout.fragment_lainnya, container, false);
+
+        imageprofile = (ImageView) _view.findViewById(R.id.iv_profile);
+        imagewallet = (ImageView) _view.findViewById(R.id.iv_wallet);
+        imagepengaturan = (ImageView) _view.findViewById(R.id.iv_pengaturan);
+        imagebantuan = (ImageView) _view.findViewById(R.id.iv_bantuan);
+        imageketentuan = (ImageView) _view.findViewById(R.id.iv_ketentuan);
+        imagelogout = (ImageView) _view.findViewById(R.id.iv_logout);
+        txtprofile = (TextView) _view.findViewById(R.id.tv_profile);
+        txtwallet = (TextView) _view.findViewById(R.id.tv_wallet);
+        txtpengaturan = (TextView) _view.findViewById(R.id.tv_pengaturan);
+        txtbantuan = (TextView) _view.findViewById(R.id.tv_bantuan);
+        txtketentuan = (TextView) _view.findViewById(R.id.tv_ketentuan);
+        txtlogout = (TextView) _view.findViewById(R.id.tv_logout);
+        context = getContext();
+
+        imageprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Profile", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imagewallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Wallet", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imagepengaturan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Pengaturan", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imagebantuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Bantuan", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imageketentuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Ketentuan", Toast.LENGTH_SHORT).show();
+            }
+        });
+        imagelogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return _view;
     }
 }
