3bb8628346e7e7a7e1493428bd58d8352d85f523
Jay Clarens <Clarens.Jay@gmail.com>
2017-07-13 22:06:29 +0700

    add Maps

--- Mobile-Member/app/src/main/java/com/TA/MVP/appmobilemember/View/Fragment/FragmentCariMap.java
+++ Mobile-Member/app/src/main/java/com/TA/MVP/appmobilemember/View/Fragment/FragmentCariMap.java
@@ -3,6 +3,7 @@ package com.TA.MVP.appmobilemember.View.Fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,19 +16,40 @@ import android.widget.Toast;
 import com.TA.MVP.appmobilemember.R;
 import com.TA.MVP.appmobilemember.View.Activity.FilterActivity;
 import com.TA.MVP.appmobilemember.View.Activity.MainActivity;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Zackzack on 10/06/2017.
  */
 
-public class FragmentCariMap extends Fragment {
+public class FragmentCariMap extends Fragment implements OnMapReadyCallback {
     private ImageButton imgcari;
     private EditText namalokasi,nama;
     private Button btnfilter;
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View _view;
+
 //    private DialogFragmentFilter dialogFragmentFilter;
+
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View _view = inflater.inflate(R.layout.fragment_cari_map, container, false);
+        _view = inflater.inflate(R.layout.fragment_cari_map, container, false);
 
         imgcari = (ImageButton) _view.findViewById(R.id.carimap_icon_cari);
         namalokasi = (EditText) _view.findViewById(R.id.carimap_et_carilokasi);
@@ -58,5 +80,29 @@ public class FragmentCariMap extends Fragment {
 
             }
         }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView= (MapView) _view.findViewById(R.id.carimap_mapview);
+        if (mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(3.6426182,98.5290616)).title("Medan").snippet("bla bla bla bal"));
+        CameraPosition Medan = CameraPosition.builder().target(new LatLng(3.6426182, 98.5290616)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Medan));
+
+
     }
 }
